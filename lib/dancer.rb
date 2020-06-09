278034e61946541c91a6480a649bc582b5b757f8
isabella g henriques <isabellagh88@gmail.com>
2020-06-09 03:26:14 +0000

    Automatically backed up by Learn

--- lib/dancer.rb
+++ lib/dancer.rb
@@ -1,13 +1,14 @@
 require_relative './class_methods_module.rb'
 require_relative './dance_module.rb'
 
+Dancer.metadata
+// returns "This class produces objects that love to dance."
 
 class Dancer
   extend FancyDance::ClassMethods
   include FancyDance::InstanceMethods
   
-  Dancer.metadata
-// returns "This class produces objects that love to dance."
+  
  
   attr_accessor :name
  

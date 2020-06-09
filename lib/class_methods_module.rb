f61923549e852d109707f02c430d2a89b1c873fe
isabella g henriques <isabellagh88@gmail.com>
2020-06-09 03:18:00 +0000

    Automatically backed up by Learn

--- lib/class_methods_module.rb
+++ lib/class_methods_module.rb
@@ -1,26 +1,27 @@
-module Dance 
-
-def twirl
-  "I'm twirling"
-end 
-
- def jump
-    "Look how high I'm jumping!"
+module FancyDance
+  module InstanceMethods
+ 
+    def twirl
+      "I'm twirling!"
+    end
+ 
+    def jump
+      "Look how high I'm jumping!"
+    end
+ 
+    def pirouette
+      "I'm doing a pirouette"
+    end
+ 
+    def take_a_bow
+      "Thank you, thank you. It was a pleasure to dance for you all."
+    end
   end
  
-  def pirouette
-    "I'm doing a pirouette"
-  end
+  module ClassMethods
  
-  def take_a_bow
-    "Thank you, thank you. It was a pleasure to dance for you all."
-  end
-
-end 
-
-module MetaDancing
- 
-  def metadata
-    "This class produces objects that love to dance."
+    def metadata
+      "This class produces objects that love to dance."
+    end
   end
 end
